1a818e4fd5c623ccf646cbdca4155e416385868f
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-03-27 23:51:45 +0100

    feat: create product activity

--- event-planner/app/src/main/java/com/example/eventplanner/ProductListPupzFragment.java
+++ event-planner/app/src/main/java/com/example/eventplanner/ProductListPupzFragment.java
@@ -3,6 +3,7 @@ package com.example.eventplanner;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -20,17 +21,32 @@ import model.Product;
 
 
 public class ProductListPupzFragment extends Fragment {
+    View view;
     FragmentProductListPupzBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_product_list_pupz, container,false);
         binding = FragmentProductListPupzBinding.inflate(getLayoutInflater());
 
+        ArrayList<Product> products = getProducts();
+
+        ProductListAdapter productListAdapter = new ProductListAdapter(requireContext(), products);
+
+        binding.productsListPupz.setAdapter(productListAdapter);
+        binding.productsListPupz.setClickable(true);
+
+        return binding.getRoot();
+    }
+
+    @NonNull
+    private static ArrayList<Product> getProducts() {
         Long[] ids = {1l, 2l, 3l, 4l, 5l};
         String[] categories = {"Category 1", "Category 2", "Category 3", "Category 4", "Category 5"};
         String[] subcategories = {"Subcategory 1", "Subcategory 2", "Subcategory 3", "Subcategory 4", "Subcategory 5"};
-        String[] names = {"Product 1", "Producgt 2", "Product 3", "Product 4", "Product 5"};
+        String[] names = {"Product 1", "Product 2", "Product 3", "Product 4", "Product 5"};
         String[] description = {"Description 1", "Description 2", "Description 3", "Description 4", "Description 5"};
         Double[] prices = {10.0, 20.0, 30.0, 40.0, 50.0};
         Double[] discounts = {1.0, 2.0, 3.0, 4.0, 5.0};
@@ -46,11 +62,6 @@ public class ProductListPupzFragment extends Fragment {
                     names[i], description[i], prices[i], discounts[i], imageIds[i], null, available[i], visible[i]));
         }
 
-        ProductListAdapter productListAdapter = new ProductListAdapter(requireContext(), products);
-
-        binding.productsListPupz.setAdapter(productListAdapter);
-        binding.productsListPupz.setClickable(true);
-
-        return binding.getRoot();
+        return products;
     }
 }
--- event-planner/app/src/main/java/com/example/eventplanner/activities/ProductsManegementActivity.java
+++ event-planner/app/src/main/java/com/example/eventplanner/activities/ProductsManegementActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -24,6 +25,7 @@ import com.example.eventplanner.ProductListPupzFragment;
 import com.example.eventplanner.R;
 import com.example.eventplanner.databinding.ActivityProductsManegementBinding;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
@@ -44,18 +46,22 @@ public class ProductsManegementActivity extends AppCompatActivity {
         String[] categories = {"Category 1", "Category 2", "Category 3", "Category 4", "Category 5"};
         String[] subcategories = {"Subcategory 1", "Subcategory 2", "Subcategory 3", "Subcategory 4", "Subcategory 5"};
 
-        Spinner categorySpinner = findViewById(R.id.product_category_filter);
-        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(ProductsManegementActivity.this, android.R.layout.simple_spinner_dropdown_item, categories);
-        categorySpinner.setAdapter(categoryAdapter);
+        TextInputLayout textInputLayout = findViewById(R.id.product_category_filter);
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) textInputLayout.getEditText();
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(ProductsManegementActivity.this, android.R.layout.simple_dropdown_item_1line, categories);
+        autoCompleteTextView.setAdapter(categoryAdapter);
 
-        Spinner subcategorySpinner = findViewById(R.id.product_subcategory_filter);
-        ArrayAdapter<String> subcategoryAdapter = new ArrayAdapter<>(ProductsManegementActivity.this, android.R.layout.simple_spinner_dropdown_item, subcategories);
-        subcategorySpinner.setAdapter(subcategoryAdapter);
+        TextInputLayout subcategoryTextInputLayout = findViewById(R.id.product_subcategory_filter);
+        AutoCompleteTextView subcategoryAutoCompleteTextView = (AutoCompleteTextView) subcategoryTextInputLayout.getEditText();
 
-        Spinner eventTypeSpinner = findViewById(R.id.event_type_filter);
+        ArrayAdapter<String> subcategoryAdapter = new ArrayAdapter<>(ProductsManegementActivity.this, android.R.layout.simple_dropdown_item_1line, subcategories);
+        subcategoryAutoCompleteTextView.setAdapter(subcategoryAdapter);
+
+        TextInputLayout eventTypeTextInputLayout = findViewById(R.id.event_type_filter);
+        AutoCompleteTextView eventTypeAutoCompleteTextView = (AutoCompleteTextView) eventTypeTextInputLayout.getEditText();
         String[] events = {"Event 1", "Event 2", "Event 3", "Event 4", "Event 5"};
-        ArrayAdapter<String> eventTypeAdapter = new ArrayAdapter<>(ProductsManegementActivity.this, android.R.layout.simple_spinner_dropdown_item, events);
-        eventTypeSpinner.setAdapter(eventTypeAdapter);
+        ArrayAdapter<String> eventTypeAdapter = new ArrayAdapter<>(ProductsManegementActivity.this, android.R.layout.simple_dropdown_item_1line, events);
+        eventTypeAutoCompleteTextView.setAdapter(eventTypeAdapter);
 
         String usedFragment = getIntent().getStringExtra("used_fragment");
 
