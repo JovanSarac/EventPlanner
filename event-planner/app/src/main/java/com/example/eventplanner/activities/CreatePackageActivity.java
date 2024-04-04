23abd09d4a6e3a0c6c5562375ceca4a2af1150ce
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-04-05 01:49:28 +0200

    fix: small fix

--- event-planner/app/src/main/java/com/example/eventplanner/activities/CreatePackageActivity.java
+++ event-planner/app/src/main/java/com/example/eventplanner/activities/CreatePackageActivity.java
@@ -16,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventplanner.R;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
@@ -36,28 +37,26 @@ public class CreatePackageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_package);
 
         String[] categories = {"Category 1", "Category 2", "Category 3", "Category 4", "Category 5"};
-        String[] subcategories = {"Subcategory 1", "Subcategory 2", "Subcategory 3", "Subcategory 4", "Subcategory 5"};
-        ArrayList<String> events = new ArrayList<>(Arrays.asList(
-                "Event 11", "Event 12", "Event 13", "Event 14", "Event 15",
-                "Event 21", "Event 22", "Event 23", "Event 24", "Event 25",
-                "Event 31", "Event 32", "Event 33", "Event 34", "Event 35",
-                "Event 41", "Event 42", "Event 43", "Event 44", "Event 45",
-                "Event 51", "Event 52", "Event 53", "Event 54", "Event 55"));
 
         TextInputLayout textInputLayout = findViewById(R.id.categories);
         AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) textInputLayout.getEditText();
         ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(CreatePackageActivity.this, android.R.layout.simple_dropdown_item_1line, categories);
         autoCompleteTextView.setAdapter(categoryAdapter);
 
-        TextInputLayout subcategoryTextInputLayout = findViewById(R.id.subcategories);
-        AutoCompleteTextView subcategoryAutoCompleteTextView = (AutoCompleteTextView) subcategoryTextInputLayout.getEditText();
-        ArrayAdapter<String> subcategoryAdapter = new ArrayAdapter<>(CreatePackageActivity.this, android.R.layout.simple_dropdown_item_1line, subcategories);
-        subcategoryAutoCompleteTextView.setAdapter(subcategoryAdapter);
+        TextInputLayout priceTextInputLayout = findViewById(R.id.price);
+        TextInputEditText editPrice = findViewById(R.id.editPrice);
+        editPrice.setFocusable(false);
+        editPrice.setClickable(false);
 
-        TextInputLayout eventTextInputLayout = findViewById(R.id.events);
-        AutoCompleteTextView eventAutoCompleteTextView = (AutoCompleteTextView) eventTextInputLayout.getEditText();
-        ArrayAdapter<String> eventAdapter = new ArrayAdapter<>(CreatePackageActivity.this, android.R.layout.simple_dropdown_item_1line, events);
-        eventAutoCompleteTextView.setAdapter(eventAdapter);
+        TextInputLayout reservationDueTextInputLayout = findViewById(R.id.reservation_due);
+        TextInputEditText editReservationDue = findViewById(R.id.edit_reservation_due);
+        editReservationDue.setFocusable(false);
+        editReservationDue.setClickable(false);
+
+        TextInputLayout cancelationDueTextInputLayout = findViewById(R.id.cancelation_due);
+        TextInputEditText editCancelationDue = findViewById(R.id.edit_cancelation_due);
+        editCancelationDue.setFocusable(false);
+        editCancelationDue.setClickable(false);
 
         ArrayList<Product> products = getProducts();
         ProductListAddAdapter productListAddAdapter = new ProductListAddAdapter(this, products);
