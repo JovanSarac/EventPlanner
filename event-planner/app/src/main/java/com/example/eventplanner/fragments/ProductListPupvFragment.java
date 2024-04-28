d70a28e116f95adc81f85331d9d7ca2bb2d84818
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-04-29 00:39:33 +0200

    feat: connecting project with firebase

--- event-planner/app/src/main/java/com/example/eventplanner/fragments/ProductListPupvFragment.java
+++ event-planner/app/src/main/java/com/example/eventplanner/fragments/ProductListPupvFragment.java
@@ -1,5 +1,6 @@
 package com.example.eventplanner.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.eventplanner.R;
 import com.example.eventplanner.activities.CreateProductActivity;
@@ -19,10 +21,18 @@ import java.util.Arrays;
 
 import com.example.eventplanner.adapters.ProductListPupvAdapter;
 import com.example.eventplanner.model.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ProductListPupvFragment extends Fragment{
     View view;
     FragmentProductListPupvBinding binding;
+    ArrayList<Product> products;
+    FirebaseFirestore db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,7 +41,9 @@ public class ProductListPupvFragment extends Fragment{
         view = inflater.inflate(R.layout.fragment_product_list_pupv, container, false);
         binding = FragmentProductListPupvBinding.inflate(getLayoutInflater());
 
-        ArrayList<Product> products = getProducts();
+        db = FirebaseFirestore.getInstance();
+
+        getProducts();
 
         ProductListPupvAdapter productListAdapter = new ProductListPupvAdapter(requireContext(), products);
 
@@ -54,39 +66,34 @@ public class ProductListPupvFragment extends Fragment{
         });
 
     }
+    private void getProducts() {
+        db.collection("Products")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for(DocumentSnapshot doc: task.getResult()){
+                            Product product = new Product(doc.getLong("id"),
+                                    doc.getLong("categoryId"),
+                                    doc.getLong("subcategoryId"),
+                                    doc.getString("name"),
+                                    doc.getString("description"),
+                                    doc.getDouble("price"),
+                                    doc.getDouble("discount"),
+                                    new ArrayList<>(), //images
+                                    new ArrayList<>(), //eventIds
+                                    doc.getBoolean("availabale"),
+                                    doc.getBoolean("visible"));
 
-    @NonNull
-    private static ArrayList<Product> getProducts() {
-        Long[] ids = {1l, 2l, 3l, 4l, 5l};
-        String[] categories = {"Category 1", "Category 2", "Category 3", "Category 4", "Category 5"};
-        String[] subcategories = {"Subcategory 1", "Subcategory 2", "Subcategory 3", "Subcategory 4", "Subcategory 5"};
-        String[] names = {"Product 1", "Product 2", "Product 3", "Product 4", "Product 5"};
-        String[] description = {"Description 1", "Description 2", "Description 3", "Description 4", "Description 5"};
-        Double[] prices = {10.0, 20.0, 30.0, 40.0, 50.0};
-        Double[] discounts = {1.0, 2.0, 3.0, 4.0, 5.0};
-        Integer[] imageIds = {R.drawable.product_1, R.drawable.product_2, R.drawable.product_3,
-                R.drawable.product_4, R.drawable.product_5};
-        ArrayList<String> events = new ArrayList<>(Arrays.asList(
-                "Event 11", "Event 12", "Event 13", "Event 14", "Event 15",
-                "Event 21", "Event 22", "Event 23", "Event 24", "Event 25",
-                "Event 31", "Event 32", "Event 33", "Event 34", "Event 35",
-                "Event 41", "Event 42", "Event 43", "Event 44", "Event 45",
-                "Event 51", "Event 52", "Event 53", "Event 54", "Event 55"));
-
-        Boolean[] available = {true, true, true, true, true};
-        Boolean[] visible = {true, true, true, true, true};
-
-        ArrayList<Product> products = new ArrayList<>();
-
-        for(int i = 0; i < ids.length; i++){
-            ArrayList<String> productEvents = new ArrayList<>();
-            for(int j = 5 * i; j < 5 * i + 5; j++){
-                productEvents.add(events.get(j));
-            }
-
-            products.add(new Product(ids[i], categories[i], subcategories[i],
-                    names[i], description[i], prices[i], discounts[i], new ArrayList<>(Arrays.asList(imageIds)), productEvents, available[i], visible[i]));
-        }
-        return products;
+                            products.add(product);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
