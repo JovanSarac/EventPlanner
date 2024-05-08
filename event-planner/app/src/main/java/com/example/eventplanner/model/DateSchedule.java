76dea8d2dad0cfcf145c1643318167831842b444
Marko Radetic <markoradetic67@gmail.com>
2024-05-08 03:58:15 +0200

    fix: DateSchedule now has schedule map.

--- event-planner/app/src/main/java/com/example/eventplanner/model/DateSchedule.java
+++ event-planner/app/src/main/java/com/example/eventplanner/model/DateSchedule.java
@@ -1,19 +1,30 @@
 package com.example.eventplanner.model;
 
 import com.example.eventplanner.utils.DateRange;
+import com.example.eventplanner.utils.Days;
+import com.example.eventplanner.utils.WorkingHours;
+
+import java.util.HashMap;
 
 public class DateSchedule {
 
     private Long id;
     private Long workerId;
     private DateRange dateRange;
+    private HashMap<Days, WorkingHours> schedule;
 
     public DateSchedule() {
         this.dateRange = null;
+
+        this.schedule = new HashMap<>();
+        for (Days day : Days.values()) {
+            this.schedule.put(day, null);
+        }
     }
 
-    public DateSchedule(Long workerId, DateRange dateRange) {
+    public DateSchedule(Long workerId, DateRange dateRange, HashMap<Days, WorkingHours> schedule) {
         this.workerId = workerId;
+        this.schedule = schedule;
         this.dateRange = dateRange;
     }
 
@@ -39,5 +50,13 @@ public class DateSchedule {
 
     public void setDateRange(DateRange dateRange) {
         this.dateRange = dateRange;
+    }
+
+    public HashMap<Days, WorkingHours> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(HashMap<Days, WorkingHours> schedule) {
+        this.schedule = schedule;
     }
 }
