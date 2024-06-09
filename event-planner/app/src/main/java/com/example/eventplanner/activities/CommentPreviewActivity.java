93cbfa7212d4e966bf85633fd10dfcba0f79f8b9
Marko Radetic <markoradetic67@gmail.com>
2024-06-09 21:04:05 +0200

    feat: Added display for pupv comments.

---
+++ event-planner/app/src/main/java/com/example/eventplanner/activities/CommentPreviewActivity.java
@@ -0,0 +1,99 @@
+package com.example.eventplanner.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.eventplanner.R;
+import com.example.eventplanner.databinding.ActivityCommentPreviewBinding;
+import com.example.eventplanner.databinding.ActivityOwnerDashboardBinding;
+import com.example.eventplanner.model.Comment;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+public class CommentPreviewActivity extends AppCompatActivity {
+
+    ActivityCommentPreviewBinding binding;
+    LinearLayout commentsContainer;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_comment_preview);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding = ActivityCommentPreviewBinding.inflate(getLayoutInflater());
+
+        commentsContainer = binding.commentsContainer;
+
+        binding.backBtn.setOnClickListener(v->{
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void retrieveCommentsByCompanyId(String companyId){
+        db.collection("Comment")
+                .whereEqualTo("companyId", companyId)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots ->{
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        displayComment(document.toObject(Comment.class));
+                    }
+                });
+    }
+
+    private void displayComment(Comment comment){
+        View commentView = LayoutInflater.from(this).inflate(R.layout.comment_card, commentsContainer, false);
+
+        TextView commentGradeTextView = commentView.findViewById(R.id.comment_grade_value);
+        commentGradeTextView.setText(comment.getGrade());
+
+        TextView commentDescriptionTextView = commentView.findViewById(R.id.comment_description_value);
+        commentDescriptionTextView.setText(comment.getDescription());
+
+        MaterialButton showReportCommentButton = commentView.findViewById(R.id.show_report_comment_btn);
+        LinearLayout commentContainer = commentView.findViewById(R.id.comment_container);
+
+        EditText reportDescriptionEditText = commentView.findViewById(R.id.report_description_value);
+
+        MaterialButton reportCommentButton = commentView.findViewById(R.id.report_comment_btn);
+
+        showReportCommentButton.setOnClickListener(v -> {
+            if (commentContainer.getVisibility() == View.GONE) {
+                commentContainer.setVisibility(View.VISIBLE);
+            } else {
+                commentContainer.setVisibility(View.GONE);
+            }
+        });
+
+        reportCommentButton.setOnClickListener(v -> {
+            String explanation = reportDescriptionEditText.getText().toString();
+            if (!explanation.isEmpty()) {
+                
+            } else {
+            }
+        });
+
+        commentsContainer.addView(commentView);
+    }
+}
--- event-planner/app/src/main/java/com/example/eventplanner/model/Report.java
+++ event-planner/app/src/main/java/com/example/eventplanner/model/Report.java
@@ -1,23 +1,23 @@
 package com.example.eventplanner.model;
 
 public class Report {
-    private String status;
+    private String occurenceDate;
     private String description;
 
     public Report() {
     }
 
-    public Report(String status, String description) {
-        this.status = status;
+    public Report(String occurenceDate, String description) {
+        this.occurenceDate = occurenceDate;
         this.description = description;
     }
 
-    public String getStatus() {
-        return status;
+    public String getoOcurenceDate() {
+        return occurenceDate;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setOccurenceDate(String occurenceDate) {
+        this.occurenceDate = occurenceDate;
     }
 
     public String getDescription() {
